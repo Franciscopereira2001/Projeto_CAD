97aec5c1c1136053f205fa353d5c587e9ebbfaf0
Francisco <73718340+Franciscopereira2001@users.noreply.github.com>
2024-05-05 15:29:11 +0100

    benchrmark

--- benchmark.sh
+++ benchmark.sh
@@ -8,16 +8,28 @@ script_dir=$(dirname "$0")
 # Run the other script
 "$script_dir/compile.sh"
 
-N_EXECUCOES=10
+N_EXECUCOES=100
 
-echo -n "gg03.jss sequential:"
-./projeto_cad.exe gg03.jss sequential $N_EXECUCOES | grep Tempo | grep -o -E "([0-9]+\.[0-9]+)"
+echo "gg03.jss sequential:"
+./projeto_cad.exe gg03.jss sequential $N_EXECUCOES | grep Tempo 
+echo "==================="
 
-echo -n "gg03.jss parallel:"
-./projeto_cad.exe gg03.jss parallel $N_EXECUCOES | grep Tempo | grep -o -E "([0-9]+\.[0-9]+)"
+echo "gg03.jss parallel:"
+./projeto_cad.exe gg03.jss parallel $N_EXECUCOES | grep Tempo
+echo "==================="
 
-echo -n "ff06.jss sequential:"
-./projeto_cad.exe ff06.jss sequential $N_EXECUCOES | grep Tempo | grep -o -E "([0-9]+\.[0-9]+)"
+echo "ff06.jss sequential:"
+./projeto_cad.exe ff06.jss sequential $N_EXECUCOES | grep Tempo
+echo "==================="
 
-echo -n "ff06.jss parallel:"
-./projeto_cad.exe ff06.jss parallel $N_EXECUCOES | grep Tempo | grep -o -E "([0-9]+\.[0-9]+)"
+echo "ff06.jss parallel:"
+./projeto_cad.exe ff06.jss parallel $N_EXECUCOES | grep Tempo
+echo "==================="
+
+echo "ta80.jss sequential:"
+./projeto_cad.exe ta80.jss sequential $N_EXECUCOES | grep Tempo
+echo "==================="
+
+echo "ta80.jss parallel:"
+./projeto_cad.exe ta80.jss parallel $N_EXECUCOES | grep Tempo
+echo "==================="
